5ab0a89cadb4e0488484758196d45c441d240c15
Harika Neela <97505929+HarikaNeela@users.noreply.github.com>
2022-06-01 22:26:43 +0530

    Add files via upload

---
+++ input_sample.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+      
+        int a = scan.nextInt();
+        String mystring=scan.next();
+         int b = scan.nextInt();
+        System.out.println(a);
+        System.out.println(mystring);
+         System.out.println(b);
+        
+    }
+}
